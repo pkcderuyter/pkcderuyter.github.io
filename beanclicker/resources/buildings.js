b4f6170b6d2cc1518d22e004fcc4a153d3379ae6
Chris DeRuyter <cderuyter@pscokieffer.com>
2024-03-04 14:30:29 -0600

    Moved files under /beanclicker

--- beanclicker/resources/buildings.js
+++ beanclicker/resources/buildings.js
@@ -63,7 +63,7 @@ game.buildings.push(new Building(4, 'FooBar', 100000, 1000));
 game.buildings.push(new Building(5,'Fucks to Give',1000000,10000));
 game.buildings.push(new Building(6,'Do Teen Stuff',10000000,-100000));
 game.buildings.push(new Building(7, 'Duke Nukedem', 100000000, 100000));
-game.buildings.push(new Building(8, '₿it₿ean',1500000000, 1000000));
+game.buildings.push(new Building(8, 'BitBean',1500000000, 1000000));
 
 console.log(game.buildings);
 
@@ -174,11 +174,9 @@ function buildingClick(building) {
         }
     }
 
-    if (building.name === '₿it₿ean') {
+    if (building.name === 'BitBean') {
         if (building.purchased === 1) {
             alert('I like ₿it₿eans and I cannot lie');
-        } else if (building.purchased === 10) {
-            alert('Too the moon!');
         }
     }
 }
